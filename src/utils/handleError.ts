c87eaec652cb4e4879c854918b1d5887c5b9df61
Tyrone Onyebuagu <tyrone.onyebuagu@gmail.com>
2023-09-04 02:49:33 -0700

    updated error message

--- src/utils/handleError.ts
+++ src/utils/handleError.ts
@@ -4,7 +4,8 @@ export const handleError = (error, msg?: string) => {
   const status = error.response?.status;
   if (status === 403) {
     NotificationManager.info(
-      'To access API features please navigate to "https://cors-anywhere.herokuapp.com/corsdemo" and request for temporary access to the demo server.'
+      `To access API features please navigate to "https://cors-anywhere.herokuapp.com/corsdemo" and request for temporary access to the demo server. This
+      might take a few minutes to resolve the proxy.`
     );
   } else NotificationManager.error(msg || "An error occurred.");
 };
